b8da7c5891ae3a618ac42100be0d4ea2e038942d
Yashika SV <yashikasv8@email.com>
2026-09-20 23:18:14 +0530

    Add password visibility toggle

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+function togglePassword(inputId, button) {
+    const passwordInput = document.getElementById(inputId);
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        button.textContent = "Hide";
+    } else {
+        passwordInput.type = "password";
+        button.textContent = "Show";
+    }
+}
